8c13fa8f01434eed2809d60a10c3ec4a7ecde963
namxx054 <namxx054@umn.edu>
2021-04-09 16:15:06 +0900

    Updates showing texts  based on the clicked link

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -3,10 +3,13 @@ import React, {Component} from 'react';
 class Subject extends Component {
     render() {
       return (
-        <article>
-          <h2>{this.props.title}</h2>
+        <header>
+          <h1><a href="/" onClick={function(e){
+              e.preventDefault();
+              this.props.onChangePage();
+          }.bind(this)}>{this.props.title}</a></h1>
           {this.props.sub}
-        </article>
+        </header>
       );
     }
   }
