3ddf19ee312329cf50cd383cc7cb84de1642d446
nam00029 <nam00029@umn.edu>
2021-04-09 12:50:15 +0900

    Add states

--- src/App.js
+++ src/App.js
@@ -6,11 +6,28 @@ import Content from './components/Content';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject:{title:"Web", sub:"World Wide Web"},
+      contents:[
+        {id:1, title:"HTML", desc:"HTML is ..."},
+        {id:2, title:"CSS", desc:"CSS is ..."},
+        {id:3, title:"JavaScript", desc:"JavaScript is ..."}
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Subject title="Web" sub="World Wide Web"></Subject>
-        <TOC></TOC>
+        <Subject 
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+        </Subject>
+        <TOC
+          data={this.state.contents}>
+        </TOC>
         <Content title="HTML" desc="HTML is..."></Content>
       </div>
     );
